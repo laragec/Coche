d609d511b28eafd875580004654d21621eeffa42
laragec <guillermolara10@hotmail.com>
2022-01-21 18:13:43 +0100

    Commit1:Pedir Datos Usuario

--- src/main/java/poo/CochePrincipal.java
+++ src/main/java/poo/CochePrincipal.java
@@ -1,11 +1,17 @@
 package poo;
+import javax.swing.JOptionPane;
 public class CochePrincipal {
     public static void main(String[] args) {
         Coche Renault = new Coche();
-        Renault.setColor("verde");
-        Renault.setAsientos("si");
-        Renault.setClimatizador("no");
+        String color;
+        String asientos;
+        String clima;
+        color=JOptionPane.showInputDialog("Introduzca el color del coche:");
+        asientos=JOptionPane.showInputDialog("¿El coche tiene asientos de cuero?");
+        clima=JOptionPane.showInputDialog("¿El coche tiene climatizador?");
+        Renault.setColor(color);
+        Renault.setAsientos(asientos);
+        Renault.setClimatizador(clima);
         System.out.println(Renault.getDatos());
     }
-    
 }
